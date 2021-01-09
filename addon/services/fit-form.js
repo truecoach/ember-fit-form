11975f8c541005aafb6c3f6c2bab8fe2042d3852
Steve Szczecina <steve@truecoach.co>
2021-01-08 23:20:20 -0700

    [refactor] es class service

--- addon/services/fit-form.js
+++ addon/services/fit-form.js
@@ -5,16 +5,15 @@ import { set, get } from '@ember/object';
 import { dasherize } from '@ember/string';
 import { getOwner } from '@ember/application';
 
-export default Service.extend({
+export default class FitFormService extends Service {
   /**
    * Cached adapters to reduce multiple expensive lookups.
    *
    * @property _adapters
    * @private
    * @type Object
-   * @default null
    */
-  _Adapters: null,
+  _Adapters = {};
 
   /**
    * When the Service is created, activate adapters that were specified in the
@@ -25,15 +24,15 @@ export default Service.extend({
    * @param {Void}
    * @return {Void}
    */
-  init() {
+  constructor() {
+    super(...arguments);
+
     const owner = getOwner(this);
-    owner.registerOptionsForType('ember-fit-form@form-adapter', { instantiate: false });
+    owner.registerOptionsForType('ember-fit-form@form-adapter', {
+      instantiate: false,
+    });
     owner.registerOptionsForType('form-adapter', { instantiate: false });
-
-    set(this, '_Adapters', {});
-
-    this._super(...arguments);
-  },
+  }
 
   /**
    * Looks up the adapter from the container. Prioritizes the consuming app's
@@ -50,11 +49,17 @@ export default Service.extend({
     const cachedAdapters = this._Adapters;
     const cachedAdapter = get(cachedAdapters, name);
 
-    if (cachedAdapter) { return cachedAdapter; }
+    if (cachedAdapter) {
+      return cachedAdapter;
+    }
 
     const dasherizedAdapterName = dasherize(name);
-    const availableAdapter = getOwner(this).lookup(`ember-fit-form@form-adapter:${dasherizedAdapterName}`);
-    const localAdapter = getOwner(this).lookup(`form-adapter:${dasherizedAdapterName}`);
+    const availableAdapter = getOwner(this).lookup(
+      `ember-fit-form@form-adapter:${dasherizedAdapterName}`
+    );
+    const localAdapter = getOwner(this).lookup(
+      `form-adapter:${dasherizedAdapterName}`
+    );
     const adapter = localAdapter ? localAdapter : availableAdapter;
 
     assert(`[fit-form] Could not find a form adapter named ${name}`, adapter);
@@ -63,4 +68,4 @@ export default Service.extend({
 
     return adapter;
   }
-});
+}
